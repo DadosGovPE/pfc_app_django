acf92ed11cbb7170c268b5a219788232fe4875db
Gilberto <g.trindade@gmail.com>
2024-07-23 01:24:57 -0300

    adicionando o user ao log do nginx

--- pfc_app_django/settings.py
+++ pfc_app_django/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'pfc_app_django.middleware.LogUsernameMiddleware',
+    'middleware.LogUsernameMiddleware',
     
 ]
 
