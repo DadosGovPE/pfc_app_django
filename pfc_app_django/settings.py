8d394f0e2f107963a2e708da8b7b071b61718cab
gilberto trindade <g.trindade@gmail.com>
2026-02-20 11:31:08 -0300

    alteracao para envio de emails

--- pfc_app_django/settings.py
+++ pfc_app_django/settings.py
@@ -198,13 +198,19 @@ AUTH_USER_MODEL = "pfc_app.User"
 # Configurações de E-mail
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = os.getenv("EMAIL_PORT")
-EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "true").strip().lower() in (
+    "1",
+    "true",
+    "yes",
+    "on",
+)
+EMAIL_TIMEOUT = int(os.getenv("EMAIL_TIMEOUT", "30"))
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
-
-DEFAULT_FROM_EMAIL = "PFC SEPLAG <pfc.seplag@gmail.com>"
+DEFAULT_FROM_EMAIL = os.getenv(
+    "DEFAULT_FROM_EMAIL", "PFC SEPLAG <pfc.seplag@gmail.com>"
+)
 
 
 JAZZMIN_SETTINGS = {
